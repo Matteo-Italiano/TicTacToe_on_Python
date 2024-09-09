df55fea146ea8c39fc99684a2ff75618d4f2e39f
Matteo Italiano <matteo.italiano@3ap.ch>
2024-09-09 09:35:53 +0200

    Parameter

--- TictacToe.py
+++ TictacToe.py
@@ -1,92 +1,75 @@
-a = 1
-b = 2
-c = 3 
-d = 4
-e = 5
-f = 6 
-g = 7 
-h = 8 
-i = 9
+
+a, b, c = 1, 2, 3
+d, e, f = 4, 5, 6
+g, h, i = 7, 8, 9
 
 
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
-#This is the Wrong Code!
+# DO NOT TOUCH THIS ONE WORKS
 
-print(f" >>> {a} {b} {c}\n >>> {d} {e} {f}\n >>> {g} {h} {i}")
-print("Wähle ein Feld aus: ")
-Inputt = int(input())
-if Inputt == a:
-    a += 10
-if Inputt == b:
-    b += 10
-if Inputt == c:
-    c += 10
-if Inputt == d:
-    d += 10
-if Inputt == e:
-    e += 10
-if Inputt == f:
-    f += 10
-if Inputt == g:
-    g += 10
-if Inputt == h:
-    h += 10
-if Inputt == i:
+
+
+einzelinput = input(f"Wie viele Spieler(1/2)?")
+if einzelinput == "2":
+    print("Viel Spass!")
     
-    i += 10
-if int(a) > 10:
-    a = "X"  
-if int(b) > 10:
-    b = "X"  
-if int(c) > 10:
-        c = "X"  
-if int(d) > 10:
-    d = "X"  
-if int(e) > 10:
-    e = "X"  
-if int(f) > 10:
-    f = "X"  
-if int(g) > 10:
-    g  = "X"  
-if int(h)  > 10:
-    h  = "X"  
-if int(i) > 10:
-    i = "X"  
+def print_Area():
+    print(f" >>> {a} {b} {c}\n >>> {d} {e} {f}\n >>> {g} {h} {i}")
 
+print_Area()
 
+current_turn = "X"
 
-    #print(f" >>> {a} {b} {c}\n >>> {d} {e} {f}\n >>> {g} {h} {i}")
-    #print("Wähle ein Feld aus: ")
+def gewinner(player):
+    # Gewinnkombinationen für den Spieler (entweder "XXX" oder "OOO")
+    if f"{a}{b}{c}" == player:
+        return True
+    if f"{d}{e}{f}" == player:
+        return True
+    if f"{g}{h}{i}" == player:
+        return True
+    if f"{a}{d}{g}" == player:
+        return True
+    if f"{b}{e}{h}" == player:
+        return True
+    if f"{c}{f}{i}" == player:
+        return True
+    if f"{a}{e}{i}" == player:
+        return True
+    if f"{g}{e}{c}" == player:
+        return True
+    return False
 
-
+while not (gewinner("XXX") or gewinner("OOO")):
     
 
+    inputFUN = input(f"Wähle ein Feld aus ({current_turn}): ")
+
+    if inputFUN == str(a):
+        a = current_turn
+    elif inputFUN == str(b):
+        b = current_turn
+    elif inputFUN == str(c):
+        c = current_turn
+    elif inputFUN == str(d):
+        d = current_turn
+    elif inputFUN == str(e):
+        e = current_turn
+    elif inputFUN == str(f):
+        f = current_turn
+    elif inputFUN == str(g):
+        g = current_turn
+    elif inputFUN == str(h):
+        h = current_turn
+    elif inputFUN == str(i):
+        i = current_turn
+    else:
+        print("Ungültige Auswahl. Versuche es erneut.")
+        
+        continue
+
+    print_Area()
+        
+
+    current_turn = "O" if current_turn == "X" else "X"
+
+print("Spiel beendet!")
